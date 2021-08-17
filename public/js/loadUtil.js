99689f547cc9833335c24260be67bfeab07eddd4
Sean Nguyen <nsean321@gmail.com>
2021-08-17 10:52:33 -0700

    pagination, list loading

--- public/js/loadUtil.js
+++ public/js/loadUtil.js
@@ -1,75 +1,11 @@
+
+
+
 const getListing = (id) => {
   axios.get(`/api/listings/id/${id}`)
     .then(({ data: listing }) => {
       console.log(listing)
     })
-}
-
-const getListings = (count) => {
-  // api call to grab count of all listings
-  // populate li to ul depending on amount of lists
-  // x amount of items per list
-  // initial call also returns the first x amount of listings
-  if (!count) { count = 1 }
-
-  axios.get(`api/listings/${count}`)
-    .then(({ data }) => {
-      let limit = 10 // 20 listings per page (consistent with api)
-      let pageCount = data.total / limit
-
-      // pagination
-      for (let i = 1; i < pageCount + 1; i++) {
-        const pageElem = document.createElement('li')
-        pageElem.className = 'waves-effect'
-        pageElem.textContent = i
-        if (i == count) {
-          pageElem.className = 'active'
-        }
-        document.getElementById('pages').append(pageElem)
-      }
-      const chevronRight = document.createElement('li')
-      chevronRight.className = "waves-effect"
-      chevronRight.innerHTML = `<i class="material-icons">chevron_right</i>`
-      document.getElementById('pages').append(chevronRight)
-
-      // clear the listings' space
-      document.getElementById('listings').innerHTML = '<h3>Recent Listings</h3>'
-      // load listings onto page
-      data.listings.forEach(listing => {
-        const title = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-        const listingElem = document.createElement('div')
-        listingElem.className = 'col s12 m4 l3 xl3'
-        listingElem.innerHTML = `
-            <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
-              <div class="card-image" style="background-image: url(${listing.image})"></div>
-              <div class="titleBox valign-wrapper">
-                <h3 class="card-title center cardTitle">${listing.title}</h3>
-              </div>
-            </div>
-          `
-        document.getElementById('listings').append(listingElem)
-      })
-    })
-
-
-  // axios.get('/api/listings')
-  //   .then(({ data: listings }) => {
-  //     listings.forEach(listing => {
-  //       const title = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
-  //       const listingElem = document.createElement('div')
-  //       listingElem.className = 'col s12 m4 l3 xl3'
-  //       listingElem.innerHTML = `
-  //         <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
-  //           <div class="card-image" style="background-image: url(${listing.image})"></div>
-  //           <div class="titleBox valign-wrapper">
-  //             <h3 class="card-title center cardTitle">${listing.title}</h3>
-  //           </div>
-  //         </div>
-  //       `
-  //       document.getElementById('listings').append(listingElem)
-  //     })
-  //   })
-  //   .catch(err => console.error(err))
 }
 
 const status = () => {
@@ -110,3 +46,67 @@ const status = () => {
   }
 }
 
+const getListings = (count) => {
+  // api call to grab count of all listings
+  // initial call also returns the first x amount of listings
+  if (!count) { count = 1 }
+
+  axios.get(`api/listings/${count}`)
+    .then(({ data }) => {
+      let limit = 5 // 20 listings per page (consistent with api)
+      let pageCount = data.total / limit
+
+      if (document.getElementById('pages').innerHTML.length < 100) {
+        // pagination
+        document.getElementById('pages').innerHTML = `
+          <li class="disabled" id="left"><i class="material-icons">chevron_left</i></li>
+          <input 
+            id="pageNumber" 
+            value='${count} of ${data.total / data.subtotal} '
+            data-pages=${data.total / data.subtotal} 
+            class="pageInput"
+          >
+          <li class="waves-effect" id="right"><i class="material-icons">chevron_right</i></li>
+        `
+      }
+
+
+      // clear the listings' space
+      document.getElementById('listings').innerHTML = ''
+      // load listings onto page
+      data.listings.forEach(listing => {
+        const title = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+        const listingElem = document.createElement('div')
+        listingElem.className = 'col s12 m4 l3 xl3'
+        listingElem.innerHTML = `
+            <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
+              <div class="card-image" style="background-image: url(${listing.image})"></div>
+              <div class="titleBox valign-wrapper">
+                <h3 class="card-title center">${listing.title}</h3>
+              </div>
+            </div>
+          `
+        document.getElementById('listings').append(listingElem)
+      })
+    })
+
+
+  // axios.get('/api/listings')
+  //   .then(({ data: listings }) => {
+  //     listings.forEach(listing => {
+  //       const title = listing.title.charAt(0).toUpperCase() + listing.title.slice(1)
+  //       const listingElem = document.createElement('div')
+  //       listingElem.className = 'col s12 m4 l3 xl3'
+  //       listingElem.innerHTML = `
+  //         <div class="card hoverable listings modal-trigger" id="cardItem" data-target="modal1" data-id=${listing.id}>
+  //           <div class="card-image" style="background-image: url(${listing.image})"></div>
+  //           <div class="titleBox valign-wrapper">
+  //             <h3 class="card-title center cardTitle">${listing.title}</h3>
+  //           </div>
+  //         </div>
+  //       `
+  //       document.getElementById('listings').append(listingElem)
+  //     })
+  //   })
+  //   .catch(err => console.error(err))
+}
