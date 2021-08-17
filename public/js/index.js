9ddb9b2258aa9e633f9faaea06210d9b92d4ce81
Sean Nguyen <nsean321@gmail.com>
2021-08-17 10:10:43 -0700

    modal, pagination, search handling updated

--- public/js/index.js
+++ public/js/index.js
@@ -20,20 +20,27 @@ const instance2 = M.Modal.init(elem2, {
   outDuration: 300
 });
 
+const elem3 = document.getElementById('pageModal');
+const instance3 = M.Modal.init(elem3, {
+  dismissible: true,
+  inDuration: 300, // Transition in duration
+  outDuration: 300
+});
+
 document.getElementById('navbarSearch').addEventListener('input', event => {
-  console.log('hit')
   if (document.getElementById('navbarSearch').value) {
-    axios.get(`/api/listings/search/${document.getElementById(navbarSearch).value.toLowerCase()}`)
+    axios.get(`/api/listings/search/${document.getElementById('navbarSearch').value.toLowerCase()}`)
       .then(({ data: listings }) => {
-        document.getElementById('listings').innerHTML = `<h3>Search Results: <span>${document.getElementById('navbarSearch').value}</span></h3>`
+        document.getElementById('spaceOne').innerHTML = `<h3>Search Results: <span>${document.getElementById('navbarSearch').value}</span></h3><ul class="pagination" id="pages"></ul>`
+        document.getElementById('listings').innerHTML = ''
         listings.forEach(listing => {
           const searchElem = document.createElement('div')
-          searchElem.classname = 'col s12 m4 l3 xl3'
+          searchElem.className = 'col s12 m4 l3 xl3'
           searchElem.innerHTML = `
             <div class="card hoverable listings modal-trigger" id="cardItem" data-target="cardModal" data-id=${listing.id}>
               <div class="card-image" style="background-image: url(${listing.image})"></div>
               <div class="titleBox valign-wrapper">
-                <h3 class="center cardTitle">${listing.title}</h3>
+                <h3 class="center card-title">${listing.title}</h3>
               </div>
             </div>
           `
@@ -42,23 +49,24 @@ document.getElementById('navbarSearch').addEventListener('input', event => {
       })
       .catch(err => console.error(err))
   } else {
-    loadUtil.getListings()
+    getListings()
   }
 })
 
 document.getElementById('sidebarSearch').addEventListener('input', event => {
   if (document.getElementById('sidebarSearch').value) {
-    axios.get(`/api/listings/search/${document.getElementById(sidebarSearch).value.toLowerCase()}`)
+    axios.get(`/api/listings/search/${document.getElementById('sidebarSearch').value.toLowerCase()}`)
       .then(({ data: listings }) => {
-        document.getElementById('listings').innerHTML = `<h3>Search Results: <span>${document.getElementById('sidebarSearch').value}</span></h3>`
+        document.getElementById('spaceOne').innerHTML = `<h3>Search Results: <span>${document.getElementById('sidebarSearch').value}</span></h3><ul class="pagination" id="pages"></ul>`
+        document.getElementById('listings').innerHTML = ''
         listings.forEach(listing => {
           const searchElem = document.createElement('div')
-          searchElem.classname = 'col s12 m4 l3 xl3'
+          searchElem.className = 'col s12 m4 l3 xl3'
           searchElem.innerHTML = `
             <div class="card hoverable listings modal-trigger" id="cardItem" data-target="cardModal" data-id=${listing.id}>
               <div class="card-image" style="background-image: url(${listing.image})"></div>
               <div class="titleBox valign-wrapper">
-                <h3 class="center cardTitle">${listing.title}</h3>
+                <h3 class="center card-title">${listing.title}</h3>
               </div>
             </div>
           `
@@ -67,13 +75,90 @@ document.getElementById('sidebarSearch').addEventListener('input', event => {
       })
       .catch(err => console.error(err))
   } else {
-    loadUtil.getListings()
+    getListings()
   }
 })
 
+document.querySelector("input")
+  .addEventListener('change', (e) => {
+    console.log(e.currentTarget.value);
+  });
 
 document.addEventListener('click', event => {
 
+  const pageCount = document.getElementById('pageNumber') && document.getElementById('pageNumber').dataset.pages
+  let currentPage = document.getElementById('pageNumber') && parseInt(document.getElementById('pageNumber').value.split(' ')[0])
+
+  // open pagination modal
+  if (event.target.id == 'pageNumber') {
+    document.getElementById('pageModal').innerHTML = `
+      <div id="pageDrop" class="pageDrop">
+        <h5>Jump to page</h5>
+        <input
+        type="number"
+        max=${pageCount} 
+        min=1 x
+        value=${currentPage} 
+        class="pageInput"
+        id="modalInput"
+        >
+        <div class="btnBox">
+          <button id="pageJump" class="modalBtn">Jump</button>
+          <button id="cancel" class="modalBtn gray">Cancel</button>
+        </div>
+      </div>
+    `
+    instance3.open()
+  }
+
+  // jump button handler
+  if (event.target.id == 'pageJump') {
+    let page = document.getElementById('modalInput').value
+    currentPage != page ? getListings(page) : instance3.close()
+
+    currentPage = page
+    document.getElementById('pageNumber').value = `${currentPage} of ${pageCount}`
+
+    document.getElementById('right').className = currentPage >= pageCount ? 'disabled' : 'waves-effect'
+    document.getElementById('left').className = currentPage <= 1 ? 'disabled' : 'waves-effect'
+
+    instance3.close()
+  }
+
+  // cancel button handler
+  if (event.target.id == 'cancel') instance3.close()
+
+  // right button handler
+  if (event.target.parentNode.id == 'right') {
+    if (event.target.parentNode.className == 'waves-effect' && currentPage < pageCount) {
+      currentPage++
+      document.getElementById('pageNumber').value = `${currentPage} of ${pageCount}`
+      getListings(currentPage)
+    }
+  }
+
+  // left button handler
+  if (event.target.parentNode.id == 'left') {
+    if (event.target.parentNode.className == 'waves-effect' && currentPage > 0) {
+      currentPage--
+      document.getElementById('pageNumber').value = `${currentPage} of ${pageCount}`
+      getListings(currentPage)
+    }
+  }
+
+  // disabled/eneble handling
+  if (document.getElementById('right')) {
+    document.getElementById('right').className = currentPage >= pageCount ? 'disabled' : 'waves-effect'
+  }
+  if (document.getElementById('left')) {
+    document.getElementById('left').className = currentPage <= 1 ? 'disabled' : 'waves-effect'
+  }
+
+
+
+
+
+  // category search
   let categoryID = event.target.id
 
   let categories = ['automotive', 'household', 'decor', 'apparel', 'accessories', 'technology', 'pet', 'random']
@@ -81,7 +166,8 @@ document.addEventListener('click', event => {
   if (categories.includes(categoryID)) {
     axios.get(`/api/listings/category/${categoryID}`)
       .then(({ data: listings }) => {
-        document.getElementById('listings').innerHTML = `<h3>${categoryID}</h3>`
+        document.getElementById('spaceOne').innerHTML = `<h3 class="category">${categoryID}</h3>`
+        document.getElementById('listings').innerHTML = ``
         listings.forEach(listing => {
           const categoryElem = document.createElement('div')
           categoryElem.className = 'col s12 m4 l3 xl3'
@@ -89,7 +175,7 @@ document.addEventListener('click', event => {
             <div class="card hoverable listings modal-trigger" id="cardItem" data-target="cardModal" data-id=${listing.id}>
               <div class="card-image" style="background-image: url(${listing.image})"></div>
               <div class="titleBox valign-wrapper">
-                <h3 class="center cardTitle">${listing.title}</h3>
+                <h3 class="center card-title">${listing.title}</h3>
               </div>
             </div>
           `
@@ -99,6 +185,7 @@ document.addEventListener('click', event => {
       .catch(err => console.error(err))
   }
 
+  // signOut
   if ((event.target.classList.contains('signOut')) && (localStorage.getItem('token'))) {
     localStorage.removeItem('token')
     window.location = './index'
