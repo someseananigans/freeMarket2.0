1f44dfa50a979f34a6c66e9e327d5b2428d4e9a8
Sean Nguyen <nsean321@gmail.com>
2021-11-09 15:34:31 -0800

    refactor to react (Initial)

--- server.js
+++ server.js
@@ -3,14 +3,16 @@ let PORT = 3001
 
 const express = require('express')
 const { join } = require('path')
-const sequelize = require('./db')
-const passport = require('passport')
+// Database
+const syncDB = require('./db')
 const { User, Listing } = require('./models')
+// Authentication / Strategy
+const passport = require('passport')
 const { Strategy: JWTStrategy, ExtractJwt } = require('passport-jwt')
 
 const app = express()
 
-app.use(express.static(join(__dirname, 'public')))
+app.use(express.static(join(__dirname, 'client', 'build')))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
@@ -22,19 +24,25 @@ passport.use(User.createStrategy())
 passport.serializeUser(User.serializeUser())
 passport.deserializeUser(User.deserializeUser())
 
-let opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.SECRET
+let opts = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.SECRET
+}
 
-passport.use(new JWTStrategy(opts, (jwt_payload, cb) => User.findOne({ id: jwt_payload, include: [Listing] })
-  .then(user => cb(null, user))
-  .catch(err => cb(err))))
+passport.use(new JWTStrategy(opts, (jwt_payload, cb) => {
+  User.findOne({ id: jwt_payload, include: [Listing] })
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
 
 app.use(require('./routes'))
 
 // { force: true }
-sequelize.sync({})
-  .then(() => app.listen(process.env.PORT || PORT))
+syncDB.sync({})
+  .then(() => {
+    console.log("server running")
+    app.listen(process.env.PORT || PORT)
+  })
   .catch(err => console.log(err))
 
 
