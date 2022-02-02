4b6e3040d44cefddbc37dea6efa34778b1462e01
Sean Nguyen <nsean321@gmail.com>
2022-02-02 00:03:39 -0800

    created navbar (basic)

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,112 @@
-import { useState, useEffect } from 'react';
-import { useHistory } from 'react-router-dom'
+import { useState } from 'react';
+import {
+  styled,
+  alpha
+} from '@mui/material/styles';
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  Badge,
+} from '@mui/material/';
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+} from '@mui/icons-material/';
+import { Drawer } from '..';
 
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  '&:hover': {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(3),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+}));
 
 const Navbar = () => {
-  
+  const [drawer, setDrawer] = useState(false)
+  const toggleDrawer = () => setDrawer(!drawer)
+
+
   return (
     <>
-      
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              MUI
+            </Typography>
+            <Search>
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+              <StyledInputBase
+                placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </Search>
+            <Box sx={{ flexGrow: 1 }} />
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 0 }}
+              onClick={toggleDrawer}
+            >
+              <Badge badgeContent={17} color="error">
+                <MenuIcon />
+              </Badge>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+
+      </Box>
+      <Drawer
+        open={drawer}
+        toggle={toggleDrawer}
+      />
     </>
   );
 }
