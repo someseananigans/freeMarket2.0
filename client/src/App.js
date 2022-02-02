b8657846341c97fc5920ecbd090b1cb4533fcf60
Sean Nguyen <nsean321@gmail.com>
2022-02-02 00:03:04 -0800

    introduced authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,34 @@ import {
   Route,
   Switch
 } from 'react-router-dom'
-import { Home } from './pages'
+import { Home, Auth } from './pages'
+
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
 
 
 const App = () => {
 
   return (
-    <div id="main">
-      <Switch>
-        <Route exact path='/' >
-          <Home />
-        </Route>
-        {/* <Page404 /> */}
-      </Switch>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <div id="main">
+        <Switch>
+          <Route exact path='/' >
+            <Home />
+          </Route>
+          <Route exact path='/auth' >
+            <Auth />
+          </Route>
+          {/* <Page404 /> */}
+        </Switch>
+      </div>
+    </ThemeProvider>
   )
 }
 
