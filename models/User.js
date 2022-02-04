9605316ab80394532f2e63d6e1474199ff4b4ab9
Sean Nguyen <nsean321@gmail.com>
2022-02-03 18:09:07 -0800

    add id and username to model

--- models/User.js
+++ models/User.js
@@ -4,10 +4,20 @@ const sequelize = require('../db')
 
 // establish user table (syntax for autherization purposes)
 const User = pls.defineUser(sequelize, {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
   name: DataTypes.STRING,
   email: {
     type: DataTypes.STRING,
-    unique: true 
+    unique: true
   },
   phone: DataTypes.STRING
 })
