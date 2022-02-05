67db499838b1aa4f89729607691e070d3cc9224b
Sean Nguyen <nsean321@gmail.com>
2022-02-05 13:03:48 -0800

    userContext handling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,35 @@
-// import { } from '../components'
+import { Navbar } from '../components'
 // import '../index.css'
-
+import { useState, useContext, useEffect } from 'react'
+import { UserContext } from '../utils/Context/';
+import { useHistory } from 'react-router-dom'
+import { User } from '../utils/API'
 
 const Home = () => {
+  const { customer, setCustomer } = useContext(UserContext)
+
+  const history = useHistory()
+  // const [state, setState] = useState(false)
+  // const toggleDraw = () => setState(!state)
+
+
+
+  const capFirst = (name) => {
+    const names = name.split(' ')
+    for (let index = 0; index < names.length; index++) {
+      names[index] = names[index][0].toUpperCase() + names[index].slice(1)
+    }
+    return names.join(' ')
+  }
+
+
+
   return (
     <>
-      <h1>test</h1>
+
+      <Navbar />
+      {customer.name && <h1 onClick={console.log(customer)}>{'Welcome back, ' + capFirst(customer.name)}</h1>}
+
     </>
   )
 }
