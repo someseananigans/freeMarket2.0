fbac4f7fa1a680428733e88db63d7e9b38e3f0e3
Sean Nguyen <nsean321@gmail.com>
2022-02-06 13:18:29 -0800

    update listing model for full needs

--- models/Listing.js
+++ models/Listing.js
@@ -3,16 +3,35 @@
 const { model, Schema } = require('mongoose')
 
 const ListingSchema = new Schema({
-  isArchive: Boolean,
+  isArchive: {
+    type: Boolean,
+    default: false
+  },
   title: {
     type: String,
     required: true
   },
+  photos: {
+    type: String,
+    required: true
+  },
   description: String,
-  categories: [{
+  hashTag: [String],
+  category: {
     type: Schema.Types.ObjectId,
     ref: 'category'
-  }],
+  },
+  condition: {
+    type: String,
+    required: true
+  },
+  color: String,
+  shippingZip: Number,
+  shippingFee: String,
+  price: Number,
+  sellingFee: Number,
+  deliveryCharge: Number,
+  earning: Number,
   comments: [{
     type: Schema.Types.ObjectId,
     ref: 'comment'
